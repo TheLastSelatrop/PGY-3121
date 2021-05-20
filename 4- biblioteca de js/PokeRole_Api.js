c0667d36f2621a0ca943eb0e142c80b9e86e4b20
Liuck929 <ag.figueroa@duocuc.cl>
2021-05-20 11:38:33 -0400

    Agustin- Organizado de archivos
    Organizado de archivos  Personajes API.js
    
    Agregado Archivos Mantenimiento para Api GEO

---
+++ 4- biblioteca de js/PokeRole_Api.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    $('#traer-pk').click(function(){
+        //Pokemon 1er llamado a la lista
+        $.get({
+            url: 'https://pokeapi.co/api/v2/pokemon?limit=6',
+            success: function(listaPK) {
+ 
+             var tarjetas = $('#tarjetas')
+             tarjetas.empty();
+ 
+             console.log(listaPK.results)
+
+             //Por cada elemento de la lista, otro llamado
+             $.each(listaPK.results, function(indice, elemento){
+                $.get({
+                    url: elemento.url,
+                    success: function(detalle) {
+                        console.log(detalle)
+                        tarjetas.append("<div class='card'>"+
+                         "<img src='" + detalle.sprites.front_default + "' class='card-img-top' alt='" + elemento.name + "'>"+
+                         "<div class='card-body'>"+
+                             "<h5 class='card-title'>" + detalle.name + "</h5>"+
+                             "<span class='card-text'>" + "HP: EXAMPLE BOTTOM TEXT" + "</span> <br>"+
+                             "<span class='card-text'>" + "AT: EXAMPLE BOTTOM TEXT" + "</span> <br>"+
+                             "<span class='card-text'>" + "DF: EXAMPLE BOTTOM TEXT" + "</span> <br>"+
+                             "<span class='card-text'>" + "SP: EXAMPLE BOTTOM TEXT" + "</span> <br>"+
+                         "</div>"+
+                        "</div>");
+
+                    },
+                    error: function(error) {
+                        console.error(error);
+                    }
+                });
+
+                 
+                 
+                     
+             });
+
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        })
+     })
+})
